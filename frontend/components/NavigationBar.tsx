42c1a00ed6561ab2ac5390cb771f5e227175268c
alenjannn <allenCoding1721@gmail.com>
2025-10-25 22:16:20 +0800

    feat: Fixed the bar in the app

--- frontend/components/NavigationBar.tsx
+++ frontend/components/NavigationBar.tsx
@@ -26,7 +26,7 @@ export default function NavigationBar({ tabs, activeTab, onTabChange }: Navigati
       case 'alerts':
         return (
           <div className="w-6 h-6 flex items-center justify-center">
-            <svg className="w-6 h-6 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
             </svg>
           </div>
